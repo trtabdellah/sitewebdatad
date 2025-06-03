145d472f953fc5071d62eca78d77662ea09fd940
fzfzfdsersa456 <touratiabdo@gmail.com>
2025-06-03 14:51:02 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+<?php
+    include 'components/Navigation__Bar.php';
+    // ===========Condition==============
+        if(isset($_SESSION['IS_LOGGIN'])){
+            if($_SESSION['ROLE'] == 0){
+                echo "<script>window.location='pages/Dashboard.php?type=n'</script>";
+            }else{
+                echo "<script>window.location='pages/Customers.php?type=n'</script>";
+            }
+        }
+    // ========X===Condition===x=========   
+?>
+
+    <!-- ---------------Home Page--------------- -->
+       <div class="container-fluid d-flex justify-content-center align-items-center" id="home__page">
+           <div class="row">
+               <div class="col-12 text-center">
+                    <h2>Welcome to the</h2>
+                    <h1>Spark Foundation Bank</h1>
+                    <a href="<?php echo SITE__PATH; ?>/pages/Login.php?type=n"><button class="btn">Login Now</button></a>
+               </div>
+           </div>
+       </div>
+    <!-- ------------X---Home Page---X------------ -->
+<?php
+    include 'components/Footer.php';
+?>
